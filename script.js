e3d845cbc439442d008a532fa260bd56eda7871a
Mohd hadi <Zaidihadi440@gmail.com>
2024-05-20 14:33:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let currentplayer = "X";
+
+let arr = Array(9).fill(null);
+
+function winnerplayer(){
+  if(
+    (arr[0] !== null && arr[0] == arr[1] && arr[1] == arr[2]) || 
+    (arr[3] !== null && arr[3] == arr[4] && arr[4] == arr[5]) || 
+    (arr[6] !== null && arr[6] == arr[7] && arr[7] == arr[8]) ||
+    (arr[0] !== null && arr[0] == arr[3] && arr[3] == arr[6]) ||
+    (arr[1] !== null && arr[1] == arr[4] && arr[4] == arr[7]) ||
+    (arr[2] !== null && arr[2] == arr[5] && arr[5] == arr[8]) ||
+    (arr[0] !== null && arr[0] == arr[4] && arr[4] == arr[8]) ||
+    (arr[2] !== null && arr[2] == arr[4] && arr[4] == arr[6])
+  ){
+    document.write(`Winner ${currentplayer}`)
+    return;
+    
+  }
+  if(!arr.some(e => e === null)){
+    document.write(`Drow!!`)
+    return;
+  }
+
+}
+
+function heandelclick(el){
+  let id  = Number(el.id);
+  if(arr[id] !== null){
+    return
+  }
+  arr[id] = currentplayer;
+  el.innerText = currentplayer;
+  winnerplayer();
+  currentplayer = currentplayer == "X" ? "0" : "X";
+ 
+}
